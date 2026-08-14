ee44840fc016936fad114f1ffd33f156a16b9b88
gyeongbibin <151108886+gyeongbibin@users.noreply.github.com>
2026-08-14 16:21:46 +0900

    feat: N으로 표현 문제풀이

---
+++ Lv3/N으로표현.js
@@ -0,0 +1,26 @@
+function solution(N, number) {
+    const valuesByCount = Array.from({ length: 9 }, () => new Set());
+
+    for (let count = 1; count <= 8; count++) {
+        valuesByCount[count].add(Number(String(N).repeat(count)));
+
+        for (let leftCount = 1; leftCount < count; leftCount++) {
+            const rightCount = count - leftCount;
+
+            for (const left of valuesByCount[leftCount]) {
+                for (const right of valuesByCount[rightCount]) {
+                    valuesByCount[count].add(left + right);
+                    valuesByCount[count].add(left - right);
+                    valuesByCount[count].add(left * right);
+                    if (right !== 0) {
+                        valuesByCount[count].add(Math.trunc(left / right));
+                    }
+                }
+            }
+        }
+
+        if (valuesByCount[count].has(number)) return count;
+    }
+
+    return -1;
+}
